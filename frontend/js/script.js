5f8a4a59f33a56db8866c8e7d3194f145980154f
Maxim <maximdeclient@gmail.com>
2024-02-09 07:01:59 +0100

    Implemented skin upload by name

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -3,7 +3,11 @@ const container = document.getElementById('skin-viewer');
 const downloadButton = document.getElementById('download');
 const inputField = document.getElementById('username');
 downloadButton.disabled = true;
-let skinURL = null;
+let skinURL = "";
+let selectedSkinURL = "";
+const paintEntries = document.querySelectorAll('.paint-type.card .paint-entry');
+const fileInput = document.querySelector('input[type="file"]');
+let timeoutId = null;
 
 const skinViewer = new skinview3d.SkinViewer({
     canvas: container,
@@ -13,30 +17,16 @@ const skinViewer = new skinview3d.SkinViewer({
     enableControls: true
 });
 
-skinViewer.zoom = 0.8;
-
+skinViewer.zoom = 0.7;
 skinViewer.controls.enableZoom = false
 skinViewer.autoRotate = true;
 skinViewer.animation = new skinview3d.RunningAnimation();
 skinViewer.animation.speed = 0.1;
 
-const paintEntries = document.querySelectorAll('.paint-type.card .paint-entry');
-
-
-let timeoutId = null;
-
-const fileInput = document.querySelector('input[type="file"]');
-
 inputField.addEventListener('input', (event) => {
     clearTimeout(timeoutId);
 
     const inputValue = event.target.value;
-
-    if (inputValue.length <= 3) {
-        showNotification("Username is too short!");
-        event.target.value = inputValue.slice(0, 16);
-        return;
-    }
 
     if (inputValue.length > 16) {
         showNotification("Username is too long!");
@@ -51,11 +41,25 @@ inputField.addEventListener('input', (event) => {
     }
 
     timeoutId = setTimeout(() => {
+
+        if(!inputValue) {
+            showNotification("Empty input!");
+            return;
+        }
+
+        if (inputValue.length <= 2) {
+            showNotification("Username is too short!");
+            event.target.value = inputValue.slice(0, 16);
+            return;
+        }
+
         const url = "https://mineskin.eu/skin/" + inputValue;
+        clearFileInput(fileInput);
+        selectedSkinURL = url;
         skinViewer.loadSkin(url);
-        this.skinURL = url;
-        fileInput.value = '';
-    }, 2000);
+        downloadButton.disabled = true;
+        skinURL = "";
+    }, 1000);
 
 });
 
@@ -63,7 +67,6 @@ inputField.addEventListener('input', (event) => {
 fileInput.addEventListener('change', (event) => {
     const file = event.target.files[0];
     if (file) {
-
         if (!file.type.startsWith('image/png')) {
             showNotification("Selected file is not a PNG.", false);
             clearFileInput(event.target);
@@ -80,7 +83,10 @@ fileInput.addEventListener('change', (event) => {
         reader.onload = function (e) {
             const skinDataUrl = e.target.result;
             skinViewer.loadSkin(skinDataUrl);
+            downloadButton.disabled = true;
             inputField.value = '';
+            skinURL = "";
+            selectedSkinURL = "";
         };
         reader.readAsDataURL(file);
     } else {
@@ -93,16 +99,27 @@ function clearFileInput(input) {
 }
 
 paintEntries.forEach(paintEntry => {
-    paintEntry.addEventListener('click', () => {
+    paintEntry.addEventListener('click', async () => {
         console.log("Converting skin " + paintEntry.id);
 
-        if (!fileInput.files || !fileInput.files.length) {
-            showNotification("Please upload a skin first!", false);
+        if ((!selectedSkinURL || selectedSkinURL === "") && (!fileInput.files || !fileInput.files.length)) {
+            showNotification("Please upload a skin or type your name in first", false);
             return;
         }
 
-        const formData = new FormData();
-        formData.append('skinFile', fileInput.files[0]);
+        let formData = new FormData();
+        let file;
+
+        if (selectedSkinURL && selectedSkinURL !== "") {
+            file = await urlToFile(selectedSkinURL);
+        } else if (fileInput.files && fileInput.files.length) {
+            file = fileInput.files[0];
+        } else {
+            showNotification("Please upload a skin or type your name in first", false);
+            return;
+        }
+
+        formData.append('skinFile', file);
         fetch('https://toilet-api.botpanel.de/upload', {
             method: 'POST',
             body: formData
@@ -157,7 +174,7 @@ paintEntries.forEach(paintEntry => {
 });
 
 downloadButton.addEventListener('click', function() {
-    if (skinURL || downloadButton.disabled) {
+    if (skinURL && skinURL !== "") {
         downloadImage(skinURL);
     } else {
         showNotification("Could not find requested Skin! Generate a new one.", false);
@@ -176,7 +193,6 @@ async function downloadImage(imageSrc) {
     link.click()
     document.body.removeChild(link)
 }
-
 
 function showNotification(message, isSuccess) {
     const notificationContainer = document.getElementById('notification-container');
@@ -210,4 +226,22 @@ function showNotification(message, isSuccess) {
     }, 3000);
 }
 
+async function urlToFile(url) {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.crossOrigin = 'Anonymous';
+        img.onload = function() {
+            const canvas = document.createElement('canvas');
+            const ctx = canvas.getContext('2d');
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0);
+            canvas.toBlob(blob => {
+                resolve(new File([blob], 'image.png', { type: 'image/png' }));
+            }, 'image/png');
+        };
+        img.onerror = reject;
+        img.src = url;
+    });
+}
 
